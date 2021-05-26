1bef3e541d28918398141504e86a5d2f170a7f0f
Ethan Tracy <ethanjet500@gmail.com>
2021-05-25 21:06:48 -0400

    Implement actual physics

--- Ball.py
+++ Ball.py
@@ -6,12 +6,13 @@ import math
 
 class Ball(pygame.sprite.Sprite):
     def __init__(
-        self, width, height, pos_x, pos_y, vel_x, vel_y, accel_x, accel_y, filePath
+        self, mass, width, height, pos_x, pos_y, vel_x, vel_y, accel_x, accel_y, filePath
     ):
         super().__init__()
         self.image = pygame.image.load(filePath)
         self.rect = self.image.get_rect()
         self.rect.center = [pos_x, pos_y]
+        self.mass = mass
         self.width = width
         self.height = height
         self.position = pygame.math.Vector2(pos_x, pos_y)
@@ -37,6 +38,11 @@ class Ball(pygame.sprite.Sprite):
         self.velocity.y += self.acceleration.y*dt
         self.position.y += (self.velocity.y * dt) + ((0.5 * self.acceleration.y) * (dt ** 2))
 
+#-------------------------------------------------------------------------------------------------
+# Takes window dimention values, as well as ball object
+# compares and flips needed values and moves ball back within walls
+#-------------------------------------------------------------------------------------------------
+
 def detect_wall_collision(windowX, windowY, ball):
     if ball.position.x > windowX - ball.width:
         ball.velocity.x = (-1 * abs(ball.velocity.x))
@@ -51,16 +57,52 @@ def detect_wall_collision(windowX, windowY, ball):
         ball.velocity.y = abs(ball.velocity.y)
         ball.velocity.y = 0
 
-def detect_ball_collision(ball1, ball2):
-    if math.hypot(ball1.position.x - ball2.position.x, ball1.position.y - ball2.position.y) < 100:
-        ball1.velocity.x = (-1 * (ball1.velocity.x))
-        ball1.velocity.y = ((ball1.velocity.y))
-        ball2.velocity.x = (-1 * (ball2.velocity.x))
-        ball2.velocity.y = ((ball2.velocity.y))
+#-------------------------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------------------------
+
+def detect_ball_collision(ball1, ball2):
+    
+    ZERO_DEGREES_VECTOR = pygame.math.Vector2(1,0)
+    
+
+    dx = ball2.position.x - ball1.position.x
+    dy = ball2.position.y - ball1.position.y
+
+    if dx == 0:
+        phi = math.pi()/2
+    else:
+        phi = math.atan(dy/dx)
+    
+    #print(phi)
+
+    cartesianAngle1 = ball1.velocity.angle_to(ZERO_DEGREES_VECTOR)
+
+    if cartesianAngle1 < 0:
+        cartesianAngle1 += 360
+
+    cartesianAngle2 = ball2.velocity.angle_to(ZERO_DEGREES_VECTOR)
+
+    if cartesianAngle2 < 0:
+        cartesianAngle2 += 360
+
+    tempVel1X = ( ( (ball1.velocity.length() * math.cos(cartesianAngle1 - phi)) * (ball1.mass - ball2.mass) ) + (2 * ball2.mass * ball2.velocity.length() * math.cos(cartesianAngle1)) )
+    tempVel1Y = ball1.velocity.length()
+
+
+    #tempVector1 = pygame.math.Vector2((),())
+    #tempVector2 = pygame.math.Vector2()
+
+    #print(cartesianAngle1)
+    #print(cartesianAngle2)
+
+    
+
+#-------------------------------------------------------------------------------------------------
 
 def main():
 
+    
     FPS = 60
 
     clock = pygame.time.Clock()
@@ -70,8 +112,8 @@ def main():
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    ball = Ball(100, 100, 450, 300, 10, 0, 0, .1, os.path.join("Assets", "ball2.png"))
-    ball2 = Ball(100, 100, 200, 200, -10, 0, 0, .1, os.path.join("Assets", "ball.png"))
+    ball = Ball(1, 100, 100, 450, 300, 0, 0, 0, 0, os.path.join("Assets", "ball2.png"))
+    ball2 = Ball(1, 100, 100, 200, 200, -10, 0, 0, .1, os.path.join("Assets", "ball.png"))
 
     ball_group = pygame.sprite.Group()
     ball_group.add(ball, ball2)
